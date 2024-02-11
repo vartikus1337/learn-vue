ef8a6b9365d95937c9a5c398aa2b1e828063dfc0
vartikus1337 <pasha_klevtsov@vk.com>
2024-02-11 15:19:54 +0300

    sticky footer

--- ontrackType/src/App.vue
+++ ontrackType/src/App.vue
@@ -18,48 +18,6 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
-  <main>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quidem tempora doloribus,
-    quos quod voluptatibus rem ad quis fuga at libero adipisci nesciunt praesentium quam harum
-    aperiam facilis? Error, deserunt facilis quam animi id tenetur provident sit nostrum qui atque
-    voluptatum vitae maiores delectus impedit vero aliquam reiciendis distinctio voluptates rerum
-    perspiciatis minus ex laudantium eius? Et excepturi molestias beatae culpa porro odio dicta
-    doloribus labore omnis! Vero, ex iure non minus repudiandae dolorum laudantium commodi! Rerum,
-    praesentium odit maiores laudantium eius culpa maxime ad tempora iste consectetur ea sunt unde
-    vero nulla temporibus rem eaque aperiam illo odio deserunt eligendi, provident reprehenderit.
-    Consectetur quibusdam, rem blanditiis ea nobis quaerat animi nihil laudantium consequuntur
-    accusantium adipisci expedita neque magni ipsam repellendus vero sed temporibus cupiditate?
-    Deleniti incidunt inventore vel atque illo ex dignissimos sed facere eius tempora voluptatum ad
-    numquam quae ullam quia voluptates provident accusamus quam saepe, libero quis unde aliquid.
-    Praesentium magnam laborum, in ipsa molestias consectetur quaerat tenetur laudantium officiis
-    doloremque quasi corporis impedit iste nesciunt molestiae alias perspiciatis, dolorum veritatis
-    odio sapiente dignissimos tempora odit dolores! Pariatur sed vel, est odit et, maiores
-    repudiandae assumenda modi nesciunt, vero harum itaque labore sint? Placeat aperiam dolore
-    nesciunt. Minus quia hic quisquam doloremque suscipit porro? Blanditiis, impedit veritatis
-    officia commodi ad odio magnam at sequi voluptate iusto quasi qui totam quos delectus mollitia
-    optio accusamus quas sint quibusdam! Porro, assumenda quam debitis animi quo eius, cumque esse,
-    corrupti possimus quod tenetur ratione! Quae quas, itaque aliquid soluta et, ipsum nostrum dolor
-    error illum quam molestiae perspiciatis! Pariatur soluta dolor, vitae minima dolorum possimus
-    consectetur laboriosam velit, tempora architecto explicabo excepturi odit. Eveniet reiciendis
-    praesentium dolores nobis quas minus saepe distinctio aperiam, aliquid, soluta vel non repellat
-    quibusdam iusto animi ab? Eum obcaecati doloremque, suscipit necessitatibus aut deserunt, qui
-    maxime delectus libero ea id rerum sunt exercitationem! Non cumque a amet inventore fugit
-    cupiditate officiis ab nulla fugiat architecto, quo sint eum corrupti ad sapiente quaerat iure
-    dolore saepe perspiciatis sequi repudiandae consectetur error. Blanditiis porro libero
-    repudiandae deserunt iste commodi consectetur veniam temporibus harum voluptas atque molestiae,
-    repellendus natus iusto aspernatur deleniti voluptatem dignissimos, perspiciatis eos quasi odio
-    distinctio asperiores? Omnis quo eaque, similique excepturi facilis quae cumque! Asperiores,
-    neque magnam. Quia officiis vero possimus laudantium voluptas inventore eligendi sequi, modi
-    quae facilis? Officia error iure natus neque pariatur aliquam nemo autem soluta sint vitae
-    incidunt nihil illo voluptatem asperiores quo voluptas dolorum maiores rem voluptatibus ex quia
-    mollitia, laborum maxime! Ab repellat ducimus, eligendi cumque aspernatur illum veritatis
-    asperiores expedita quidem. Quas suscipit deleniti repellendus saepe. Ullam excepturi
-    consectetur eveniet blanditiis perferendis aperiam similique, quam, atque accusamus dignissimos
-    laudantium harum corporis magnam, soluta ad! Unde hic ipsam voluptatem earum illo amet vero
-    animi, nulla natus sit sint omnis rem in, repellendus reiciendis praesentium repellat tempore.
-    Nemo vero accusamus adipisci eaque natus porro! Magni excepturi nulla cumque saepe maxime quae
-    optio. Quae assumenda ipsum esse atque voluptas libero aut iusto vel aliquam fugiat, ab in
-    sapiente deleniti consequuntur?
-  </main>
-  <nav></nav>
+  <main>Lorem300</main>
+  <nav class="fixed bottom-0 z-20 bg-white text-3xl">nav</nav>
 </template>
