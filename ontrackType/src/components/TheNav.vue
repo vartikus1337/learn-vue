0c01e446e84bed1a056d9452779374cb1e85ba2e
vartikus1337 <pasha_klevtsov@vk.com>
2024-02-11 18:10:18 +0300

    normalize hash page (in url: /#... )

--- ontrackType/src/components/TheNav.vue
+++ ontrackType/src/components/TheNav.vue
@@ -2,30 +2,38 @@
 import { ref } from 'vue'
 import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
 import NavItem from './NavItem.vue'
 
 interface navItem {
-  title: string
+  page: string
   icon: FunctionalComponent<HTMLAttributes & VNodeProps>
 }
 
 const navItems: Array<navItem> = [
-  { title: 'timeline', icon: ClockIcon },
-  { title: 'activities', icon: ListBulletIcon },
-  { title: 'progress', icon: ChartBarIcon }
+  { page: PAGE_TIMELINE, icon: ClockIcon },
+  { page: PAGE_ACTIVITIES, icon: ListBulletIcon },
+  { page: PAGE_PROGRESS, icon: ChartBarIcon }
 ]
 
-let currentPage = ref('timeline');
+let currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1) // Срезаем 1 символ это решётка
+  if (Object.keys(navItems).includes(hash)) return hash
+  window.location.hash = PAGE_TIMELINE
+  return PAGE_TIMELINE
+}
 </script>
 
 <template>
   <nav class="fixed bottom-0 z-20 w-full bg-white text-xl">
     <ul class="flex items-center justify-around border-t text-center">
-      <NavItem 
-        v-for="{ title, icon } in navItems" 
-        :key="title" 
+      <NavItem
+        v-for="{ page: title, icon } in navItems"
+        :key="title"
         :href="`#${title}`"
-        :class="{'bg-gray-200 pointer-events-none': title == currentPage}"
+        :class="{ 'pointer-events-none bg-gray-200': title == currentPage }"
         @click="currentPage = title"
       >
         <component :is="icon" class="h-6" /> {{ title }}
---
+++ ontrackType/src/components/constants.ts
@@ -0,0 +1,5 @@
+export const PAGE_TIMELINE = 'timeline'
+
+export const PAGE_ACTIVITIES = 'activities'
+
+export const PAGE_PROGRESS = 'progress'
