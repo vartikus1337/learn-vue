cf127e5d2312ff74131c0532aa5879ef64220dd4
vartikus1337 <pasha_klevtsov@vk.com>
2024-02-11 17:02:04 +0300

    split in TheHeader on TheLogo and TheHeaderProgress

---
+++ ontrackType/src/components/TheHeaderProgress.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+<template>
+  <a href="#">
+    <div v-if="true" class="flex items-center gap-2 text-sm font-bold text-green-500">
+      Day complete!
+      <CheckCircleIcon class="h-7" />
+    </div>
+    <div v-else class="flex items-center gap-2">
+      <div class="">Progress: <span class="font-mono">20%</span></div>
+      <div class="h-3 w-3 rounded-full bg-red-600"></div>
+    </div>
+  </a>
+</template>
