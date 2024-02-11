e061fb6bc168920cf8c523d4d4354b4c2eeacd68
vartikus1337 <pasha_klevtsov@vk.com>
2024-02-11 17:53:32 +0300

    NavItem adding class (:class) and reactive var currentPage (@click)

--- ontrackType/src/components/TheNav.vue
+++ ontrackType/src/components/TheNav.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from './NavItem.vue'
@@ -13,12 +14,20 @@ const navItems: Array<navItem> = [
   { title: 'activities', icon: ListBulletIcon },
   { title: 'progress', icon: ChartBarIcon }
 ]
+
+let currentPage = ref('timeline');
 </script>
 
 <template>
   <nav class="fixed bottom-0 z-20 w-full bg-white text-xl">
-    <ul class="flex items-center justify-around border-t pt-2 text-center">
-      <NavItem v-for="{ title, icon } in navItems" :key="title" :href="`#${title}`"> 
+    <ul class="flex items-center justify-around border-t text-center">
+      <NavItem 
+        v-for="{ title, icon } in navItems" 
+        :key="title" 
+        :href="`#${title}`"
+        :class="{'bg-gray-200 pointer-events-none': title == currentPage}"
+        @click="currentPage = title"
+      >
         <component :is="icon" class="h-6" /> {{ title }}
       </NavItem>
     </ul>
